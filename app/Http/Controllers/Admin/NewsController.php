498d2fa5e0bad99de02504177031e05d990eeec6
daiki.c <math.chishima919@gmail.com>
2022-01-26 15:11:52 +0000

    laravel20

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -9,6 +9,7 @@ use App\News;
 use App\History;
 //取得した現在時刻を保存する
 use Carbon\Carbon;
+use Storage;
 
 class NewsController extends Controller
 {
@@ -27,8 +28,8 @@ class NewsController extends Controller
       
       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
@@ -78,7 +79,8 @@ class NewsController extends Controller
       if ($request->remove == 'true'){
           $news_form['image_path'] = null;
       }elseif($request->file('image')){
-          $path = $request->file('image')->store('public/image');
+          $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
+          $news->image_path = Storage::disk('s3')->url($path);
           $news_form['image_path'] = basename($path);
       }else{
           $news_form['image_path'] = $news->image_path;
